9cc03fda206a5e29a907a834f3a8b578cd5c22c2
m2gi-belaouda <abdelhalimbelaoud@gmail.com>
2023-11-13 20:13:17 +0100

    [GuruPR] Establishing the foundational structure for the project.

---
+++ GuruPR.API/Configuration/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace GuruPR.Configuration;
+
+public static class ServiceExtensions
+{
+    
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(
+            options => options.AddPolicy(
+                "CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader())
+        );
+    }
+    
+}
---
+++ GuruPR.API/Startup.cs
@@ -0,0 +1,42 @@
+﻿using GuruPR.Configuration;
+
+namespace GuruPR;
+
+public class Startup
+{
+    
+    public IConfiguration Configuration { get; }
+    
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.ConfigureCors();
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(
+            endpoint => endpoint.MapControllers()
+        );
+    }
+    
+}
