7ed431eadcc3bcb4f277b8dcf4956e8b55cfb9d2
Belaoud <58121128+belaoud001@users.noreply.github.com>
2025-05-24 21:22:35 +0200

    [Guru] Integrate Semantic Kernel (#1)
    * [Guru] Upgrade projects to .NET 9
    
    * [GuruPR-APPLICATION]: Add semantic kernel package
    
    * [Guru] Add Semantic Kernel & Register Hugging Face models
    
    * [Guru-Infrastructure] Remove unnecessary null forgiving operator

---
+++ GuruPR.Application/ModelConfigurations/HuggingFaceModelsConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace GuruPR.Application.ModelConfigurations;
+
+public class HuggingFaceModelsConfig
+{
+    public required string ApiKey { get; init; }
+    
+    public required List<HuggingFaceModelConfig> HuggingFaceModels { get; init; }
+}
