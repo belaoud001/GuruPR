2a01807924ef63c57d9d8abc8280db105e112f62
Belaoud <58121128+belaoud001@users.noreply.github.com>
2025-09-22 15:14:17 +0200

    [Guru] Integrate Azure OpenAI (#3)
    * [Guru] Integrate Azure OpenAI
    
    * [Guru-Application] Small fixes on Models configuration
    
    * [Guru] Configuration - Add service ID

---
+++ src/GuruPR.Application/ModelConfigurations/AzureOpenAIModelConfig.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace GuruPR.Application.ModelConfigurations;
+
+public class AzureOpenAIModelConfig
+{
+    public required string DeploymentName { get; [UsedImplicitly] init; }
+
+    public required string Endpoint { get; [UsedImplicitly] init; }
+
+    public required string ApiVersion { get; [UsedImplicitly] init; }
+}
---
+++ src/GuruPR.Application/ModelConfigurations/AzureOpenAIModelsConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace GuruPR.Application.ModelConfigurations;
+
+public class AzureOpenAIModelsConfig
+{
+    public required string ApiKey { get; init; }
+
+    public required List<AzureOpenAIModelConfig> AzureOpenAIModels { get; init; }
+}
