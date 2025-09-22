a522129b918c25f0d2a8d98e1474b9152b51eb75
belaoud001 <abdelhalimbelaoud@gmail.com>
2025-09-22 11:31:11 +0200

    [Guru-Application] Small fixes on Models configuration

--- src/GuruPR.Application/ModelConfigurations/AzureOpenAIModelConfig.cs
+++ src/GuruPR.Application/ModelConfigurations/AzureOpenAIModelConfig.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JetBrains.Annotations;
 
 namespace GuruPR.Application.ModelConfigurations;
 
 public class AzureOpenAIModelConfig
 {
-    public required string ModelName { get; init; }
+    public required string DeploymentName { get; [UsedImplicitly] init; }
 
-    public required string DeploymentName { get; init; }
+    public required string Endpoint { get; [UsedImplicitly] init; }
 
-    public required string Endpoint { get; init; }
-
-    public required string ApiVersion { get; init; }
+    public required string ApiVersion { get; [UsedImplicitly] init; }
 }
--- src/GuruPR.Application/ModelConfigurations/AzureOpenAIModelsConfig.cs
+++ src/GuruPR.Application/ModelConfigurations/AzureOpenAIModelsConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GuruPR.Application.ModelConfigurations;
+﻿namespace GuruPR.Application.ModelConfigurations;
 
 public class AzureOpenAIModelsConfig
 {
