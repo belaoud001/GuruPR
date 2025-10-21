b26a9bbbad96aa2f7fcecfa0c65c6c9497e1e7d1
belaoud001 <abdelhalimbelaoud@gmail.com>
2025-10-21 10:50:07 +0200

    [Guru] Add gmailing setup

---
+++ src/GuruPR.Infrastructure/Services/Email/GmailSender.cs
@@ -0,0 +1,42 @@
+﻿using GuruPR.Application.Settings.Email;
+
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+
+using MimeKit;
+
+namespace GuruPR.Infrastructure.Services.Email;
+public class GmailSender : IEmailSender
+{
+    private GmailingAppSettings _gmailingSettings;
+
+    public GmailSender(IOptions<GmailingAppSettings> gmailingSettings)
+    {
+        _gmailingSettings = gmailingSettings.Value;
+    }
+
+    public async Task SendEmailAsync(string to, string subject, string htmlBody)
+    {
+        var message = new MimeMessage();
+        message.From.Add(MailboxAddress.Parse(_gmailingSettings.Username));
+        message.To.Add(MailboxAddress.Parse(to));
+        message.Subject = subject;
+
+        message.Body = new TextPart("html")
+        {
+            Text = htmlBody
+        };
+
+        using var client = new SmtpClient();
+
+        await client.ConnectAsync(_gmailingSettings.Host, _gmailingSettings.Port, SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_gmailingSettings.Username, _gmailingSettings.AppPassword);
+
+        await client.SendAsync(message);
+
+        await client.DisconnectAsync(true);
+    }
+}
