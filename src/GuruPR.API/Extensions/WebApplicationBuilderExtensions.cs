d23b5ab9530d218ee2a2e9ca5a950d60991d89f5
belaoud001 <abdelhalimbelaoud@gmail.com>
2025-10-23 09:58:03 +0200

    [Guru - API] Modernize API configuration

---
+++ src/GuruPR.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace GuruPR.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder ConfigureAppConfiguration(this WebApplicationBuilder builder)
+    {
+        var env = builder.Environment;
+
+        builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+                             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                             .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                             .AddEnvironmentVariables();
+
+        if (env.IsDevelopment())
+        {
+            builder.Configuration.AddUserSecrets<Program>();
+        }
+
+        return builder;
+    }
+}
